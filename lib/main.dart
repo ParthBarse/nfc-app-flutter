96efec665ede4da062f0f2727d8d0f831a4b8db4
Parth Barse <parthbarse72@gmail.com>
2023-10-22 23:29:35 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
+import 'dart:async';
+import 'dart:convert';
 import 'dart:typed_data';
-import 'package:ndef/ndef.dart';
 import 'package:flutter/material.dart';
 import 'package:nfc_manager/nfc_manager.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(MyApp());
+  runApp(MaterialApp(home: MyApp()));
 }
 
 class MyApp extends StatefulWidget {
@@ -13,186 +15,239 @@ class MyApp extends StatefulWidget {
   State<StatefulWidget> createState() => MyAppState();
 }
 
-class MyAppState extends State<MyApp> {
-  ValueNotifier<dynamic> result = ValueNotifier(null);
+class MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(vsync: this, length: 2);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('NfcManager Plugin Example')),
-        body: SafeArea(
-          child: FutureBuilder<bool>(
-            future: NfcManager.instance.isAvailable(),
-            builder: (context, ss) => ss.data != true
-                ? Center(child: Text('NfcManager.isAvailable(): ${ss.data}'))
-                : Flex(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    direction: Axis.vertical,
-                    children: [
-                      Flexible(
-                        flex: 2,
-                        child: Container(
-                          margin: EdgeInsets.all(4),
-                          constraints: BoxConstraints.expand(),
-                          decoration: BoxDecoration(border: Border.all()),
-                          child: SingleChildScrollView(
-                            child: ValueListenableBuilder<dynamic>(
-                              valueListenable: result,
-                              builder: (context, value, _) =>
-                                  Text('${value ?? ''}'),
-                            ),
-                          ),
-                        ),
-                      ),
-                      Flexible(
-                        flex: 3,
-                        child: GridView.count(
-                          padding: EdgeInsets.all(4),
-                          crossAxisCount: 2,
-                          childAspectRatio: 4,
-                          crossAxisSpacing: 4,
-                          mainAxisSpacing: 4,
-                          children: [
-                            ElevatedButton(
-                                child: Text('Tag Read'),
-                                onPressed: _tagRead), //_tagRead
-                            ElevatedButton(
-                                child: Text('Ndef Write'),
-                                onPressed: _ndefWrite),
-                            ElevatedButton(
-                                child: Text('Ndef Write Lock'),
-                                onPressed: _ndefWriteLock),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ECard Wallet'),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: [
+            Tab(text: 'NFC'),
+            Tab(text: 'OCR'),
+          ],
         ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          ContentTab(),
+          OcrTab(), // Updated to OcrTab
+        ],
       ),
     );
   }
+}
+
+class ContentTab extends StatefulWidget {
+  @override
+  ContentTabState createState() => ContentTabState();
+}
+
+class ContentTabState extends State<ContentTab> {
+  ValueNotifier<dynamic> result = ValueNotifier(null);
   String ndefData = 'NDEF data will be displayed here';
 
+  TextEditingController _writeDataController = TextEditingController();
+
+  Future<String> fetchData(String data) async {
+    final url = Uri.parse(
+        'https://getcode-ndef-api.vercel.app/convert?data=${data}');
+
+    try {
+      final response = await http.get(url);
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        String message = data['textData'];
+        return message;
+      } else {
+        return "Failed to load data";
+      }
+    } catch (e) {
+      return "Failed to load data: $e";
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(height: 16), // Add margin from the top
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly, // Place buttons side by side
+          children: [
+            ElevatedButton(
+              child: Text('Tag Read'),
+              onPressed: _tagRead,
+            ),
+            ElevatedButton(
+              child: Text('Ndef Write'),
+              onPressed: () => _writeData(context),
+            ),
+          ],
+        ),
+        SizedBox(height: 16), // Add margin below buttons
+        Container(
+          margin: EdgeInsets.only(top: 48), // Adjust the margin as needed
+          child: ValueListenableBuilder<dynamic>(
+            valueListenable: result,
+            builder: (context, value, _) => Text(
+              '${value ?? ''}',
+              style: TextStyle(
+                fontSize: 24, // Adjust the font size as needed
+                fontWeight: FontWeight.bold, // Make it bold
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
   void _tagRead() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Reading NDEF Data'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              CircularProgressIndicator(), // Display a loading indicator
+              SizedBox(height: 16),
+              Text('Please hold the NFC card near the device.'),
+            ],
+          ),
+        );
+      },
+    );
+
     NfcManager.instance.startSession(onDiscovered: (NfcTag tag) async {
       Ndef? ndef = Ndef.from(tag);
-      print('Tag ID: ${tag.identifier}');
-      print('Tech List: ${tag.techList}');
       if (ndef != null) {
-      try {
-        NdefMessage message = await ndef.read();
-        setState(() {
-          ndefData = 'NDEF Data: $message';
-        });
-      } catch (e) {
-        setState(() {
-          ndefData = 'Error reading NDEF data: $e';
-        });
-      }
-    } else {
-      setState(() {
-        ndefData = 'NDEF not supported on this tag';
-      });
-    }
-      
-      result.value = tag.data;
-      print(ndefData);
+        try {
+          NdefMessage message = await ndef.read();
+          setState(() {
+            ndefData = 'NDEF Data: $message';
+          });
 
-      // print(tag.data);
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text('Tag Read'),
-          duration: Duration(seconds: 2), // Adjust the duration as needed
-        ),
-      );
+          String? ndefData1 = await fetchData(tag.data.toString());
+          result.value = ndefData1;
+
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text('Card Read Successfully.'),
+              duration: Duration(seconds: 2),
+            ),
+          );
+        } catch (e) {
+          setState(() {
+            ndefData = 'Error reading NDEF data: $e';
+          });
+        } finally {
+          Navigator.of(context).pop(); // Close the reading dialog
+        }
+      } else {
+        setState(() {
+          ndefData = 'NDEF not supported on this tag';
+        });
+        Navigator.of(context).pop(); // Close the reading dialog
+      }
       NfcManager.instance.stopSession();
     });
   }
 
-  late String query;
+  void _writeData(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Write Ndef Data'),
+          content: TextField(
+            controller: _writeDataController,
+            decoration: InputDecoration(labelText: 'Enter Ndef Data'),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text('Write'),
+              onPressed: () {
+                Navigator.of(context).pop();
+                _writeNdefData(_writeDataController.text);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 
-  // void sc() {
-  //   final TextEditingController searchController = TextEditingController();
-  //   print("bhdfbsdhbf");
-  //   showDialog(
-  //     context: context,
-  //     builder: (BuildContext context) {
-  //       return AlertDialog(
-  //         title: Text('Search'),
-  //         content: TextField(
-  //           controller: searchController,
-  //           decoration: InputDecoration(labelText: 'Enter your text'),
-  //         ),
-  //         actions: <Widget>[
-  //           TextButton(
-  //             child: Text('Cancel'),
-  //             onPressed: () {
-  //               Navigator.of(context).pop();
-  //             },
-  //           ),
-  //           TextButton(
-  //             child: Text('Search'),
-  //             onPressed: () {
-  //               query = searchController.text;
-  //             },
-  //           ),
-  //         ],
-  //       );
-  //     },
-  //   );
-  // }
+  void _writeNdefData(String data) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Writing NDEF Data'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              CircularProgressIndicator(), // Display a loading indicator
+              SizedBox(height: 16),
+              Text('Please hold the NFC card near the device for writing.'),
+            ],
+          ),
+        );
+      },
+    );
 
-  void _ndefWrite() {
-    // sc();
     NfcManager.instance.startSession(onDiscovered: (NfcTag tag) async {
       var ndef = Ndef.from(tag);
       if (ndef == null || !ndef.isWritable) {
         result.value = 'Tag is not ndef writable';
         NfcManager.instance.stopSession(errorMessage: result.value);
+        Navigator.of(context).pop(); // Close the writing dialog
         return;
       }
 
       NdefMessage message = NdefMessage([
-        NdefRecord.createUri(Uri.parse('https://flutter.dev')),
-        NdefRecord.createText("kartik"),
         NdefRecord.createMime(
-            'text/plain', Uint8List.fromList('Hello'.codeUnits)),
-        NdefRecord.createExternal(
-            'com.example', 'mytype', Uint8List.fromList('mydata'.codeUnits)),
+            'text/plain', Uint8List.fromList(data.codeUnits)),
       ]);
 
       try {
         await ndef.write(message);
-        result.value = 'Success to "Ndef Write"';
+        result.value = 'Successfully wrote NDEF data';
         NfcManager.instance.stopSession();
+        Navigator.of(context).pop(); // Close the writing dialog
       } catch (e) {
         result.value = e;
         NfcManager.instance.stopSession(errorMessage: result.value.toString());
+        Navigator.of(context).pop(); // Close the writing dialog
         return;
       }
     });
   }
+}
 
-  void _ndefWriteLock() {
-    NfcManager.instance.startSession(onDiscovered: (NfcTag tag) async {
-      var ndef = Ndef.from(tag);
-      if (ndef == null) {
-        result.value = 'Tag is not ndef';
-        NfcManager.instance.stopSession(errorMessage: result.value.toString());
-        return;
-      }
-
-      try {
-        await ndef.writeLock();
-        result.value = 'Success to "Ndef Write Lock"';
-        NfcManager.instance.stopSession();
-      } catch (e) {
-        result.value = e;
-        NfcManager.instance.stopSession(errorMessage: result.value.toString());
-        return;
-      }
-    });
+class OcrTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('OCR'),
+    );
   }
 }
